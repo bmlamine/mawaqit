77daa4e0035c2e1d07bccf44022f9e57cd5cded8
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2020-03-13 19:40:52 +0100

    support api

--- src/AppBundle/Controller/API/V2/SupportController.php
+++ src/AppBundle/Controller/API/V2/SupportController.php
@@ -3,13 +3,13 @@
 namespace AppBundle\Controller\API\V2;
 
 use AppBundle\Service\YamlLoader;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 
 /**
  * @Route("/api/2.0/support", options={"i18n"="false"})
@@ -21,8 +21,9 @@ class SupportController extends Controller
      * @Route("")
      * @Cache(public=true, maxage="86400", smaxage="86400", expires="+86400 sec")
      * @Method("GET")
-     * @param Request $request
+     * @param Request    $request
      * @param YamlLoader $yamlLoader
+     *
      * @return Response
      */
     public function randomAction(Request $request, YamlLoader $yamlLoader)
@@ -31,14 +32,20 @@ class SupportController extends Controller
 
         $support = $yamlLoader->getSupport();
 
-        if(!isset($support[$country])){
+        if (!isset($support[$country])) {
             $country = "OC";
         }
 
         $phone = array_rand($support[$country]);
 
         return new JsonResponse([
-            "deeplink"=> "whatsapp://send?phone=$phone"
+
+            "mobileApp" => [
+                "whatsapp" => "whatsapp://send?phone=$phone",
+            ],
+            "web" => [
+                "whatsapp" => "https://api.whatsapp.com/send?phone=$phone",
+            ]
         ]);
     }
 
