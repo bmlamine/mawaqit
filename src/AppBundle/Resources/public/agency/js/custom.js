93a6f2df93a78705df8dd52f78752946bd859c74
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2020-03-15 17:10:41 +0100

    new Netherlans support number open support number on whatsapp

--- src/AppBundle/Resources/public/agency/js/custom.js
+++ src/AppBundle/Resources/public/agency/js/custom.js
@@ -2,5 +2,5 @@ $(".copyright-year").text((new Date()).getFullYear());
 
 $("#whatsapp-select").bind("change keyup", function (event) {
     $("#whatsapp-phone").text($(this).val());
-    $("#whatsapp-phone").attr("href", "tel:" + $(this).val());
+    $("#whatsapp-phone").attr("href", "https://api.whatsapp.com/send?phone=" + $(this).val());
 });
