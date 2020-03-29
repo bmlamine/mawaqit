d0e53ac6fecb5f30f4f983c0e7627fae789de215
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2020-03-29 13:14:47 +0200

    improve cloudflare cache purging

--- src/AppBundle/EventListener/MosqueCacheListener.php
+++ src/AppBundle/EventListener/MosqueCacheListener.php
@@ -4,6 +4,7 @@ namespace AppBundle\EventListener;
 
 use AppBundle\Entity\Mosque;
 use AppBundle\Service\Cloudflare;
+use AppBundle\Service\RequestService;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Doctrine\ORM\Events;
@@ -15,9 +16,15 @@ class MosqueCacheListener implements EventSubscriber
      */
     private $cloudflare;
 
-    public function __construct(Cloudflare $cloudflare)
+    /**
+     * @var RequestService
+     */
+    private $requestService;
+
+    public function __construct(Cloudflare $cloudflare, RequestService $requestService)
     {
         $this->cloudflare = $cloudflare;
+        $this->requestService = $requestService;
     }
 
     public function getSubscribedEvents()
@@ -40,6 +47,10 @@ class MosqueCacheListener implements EventSubscriber
 
     private function purgeCloudFlareCache(LifecycleEventArgs $args)
     {
+        if ($this->requestService->isLocal()) {
+            return;
+        }
+
         $mosque = $args->getObject();
         if (!$mosque instanceof Mosque) {
             return;
