d9300e5d909294a43c9defce2413a0a8e7d9e96f
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2020-04-10 22:31:17 +0200

    improve dev install

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-ln -sf docker-compose.dev.yml docker-compose.yml
-docker-compose kill && docker-compose up -d --build
+docker-compose up -d --build
 docker-compose exec php composer install -n
-docker-compose exec php chmod 777 -R var/cache var/logs var/sessions web/upload
-
 echo ""
 echo "Waiting for database..."
 
@@ -14,10 +11,11 @@ done
 
 docker-compose exec php bin/console d:s:u -f
 docker-compose exec php bin/console h:f:l -n
+docker-compose exec php chmod 777 -R var/cache var/logs var/sessions web/upload
 
 echo "------------------------------------------"
 echo "Mawaqit is up"
-echo "http://localhost:10001 / login: local@local.com / password: local"
+echo "http://mawaqit.localhost:10001 / login: locale@locale.com / password: locale"
 echo "Database: host 127.0.0.1, port 10002, user root, password mawaqit"
 echo "Maildev: http://localhost:10003"
 echo "------------------------------------------"
