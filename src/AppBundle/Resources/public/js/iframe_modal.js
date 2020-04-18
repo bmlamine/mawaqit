83b7fa091f1783ce99c3a9244ebb74d12d50754a
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2020-04-18 16:57:33 +0200

    display widget on mobile view

--- src/AppBundle/Resources/public/js/iframe_modal.js
+++ src/AppBundle/Resources/public/js/iframe_modal.js
@@ -1,5 +1,5 @@
 $(".iframe-button").click(function () {
-    var iframe = '<iframe src="' + $(this).data('url') + '" frameborder="0" scrolling="no" style="width: 400px; height: 200px;"></iframe>'
+    let iframe = '<iframe src="' + $(this).data('url') + '" frameborder="0" scrolling="no" style="width: 100%; height: 260px;"></iframe>'
     $('#iframe-modal #iframe-text').text(iframe);
     $('#iframe-modal #iframe-html').html(iframe);
     $('#iframe-modal').modal();
